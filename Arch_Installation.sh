df19dbbfe092f990a7a35073cf3b707bc9aaa46d
NotIzxxY6677 <81343518+NotIzxxY6677@users.noreply.github.com>
2024-12-03 19:31:07 +0530

    Update Arch_Installation.sh

--- Arch_Installation.sh
+++ Arch_Installation.sh
@@ -34,8 +34,32 @@ mkfs.ext4 /dev/sda3
 mount --mkdir /dev/sda1 /mnt/boot
 swapon /dev/sda2
 mount /dev/sda3 /mnt
-#Install Essential Packages
+#Install Essential Packages (base system, including kernel and cpu microcode)
 pacstrap -K /mnt base linux-lts linux-firmware intel-ucode
+#Generate an fstab file (Used for Filesystem Table mount/unmount and etc.)
+genfstab -U /mnt >> /mnt/etc/fstab
+#Chroot (basically enter system without reboot to do stuff)
+arch-chroot /mnt
+#set timezone
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+hwclock --systohc
+#Install nano to do certain editing stuff. You can also install vim since it's preferred to edit sudoers file which will take place later on to create new user.
+pacman -S nano
+#Localization
+locale-gen
+nano /etc/locale.gen
+uncomment en_US.UTF-8 UTF-8 
+nano /etc/locale.conf
+LANG=en_US.UTF-8
+nano /etc/hostname
+Arch_Linux
+#Create a Strong Password for root user
+passwd
+PASSWORD
+PASSWORD
+#Enable Networking (Works for Ethernet, for any other please refer to: https://wiki.archlinux.org/title/Network_configuration)
+pacman -S dhcpcd
+systemctl enable dhcpcd
 
 #systemd-bootloader setup, inside arch-chroot after done setting up normal user account for sudo.
 
@@ -55,3 +79,5 @@ linux /vmlinuz-linux
 initrd /intel-ucode.img
 initrd /initramfs-linux.img
 options root=/dev/sda3 rw
+
+bootctl update
