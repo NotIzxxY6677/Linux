9d78e54835ddcbf17643dcda7e5c57a025a2d86c
NotIzxxY6677 <81343518+NotIzxxY6677@users.noreply.github.com>
2024-12-03 12:17:51 +0530

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -4,10 +4,10 @@
 sudo apt install -y intel-media-va-driver intel-opencl-icd intel-gpu-tools mesa-* openal-info libopenal-dev libglfw3-wayland libglfw3-dev p7zip-full vulkan-tools opencl-headers
 
 # Install KDE Plasma Desktop without recommended packages
-sudo apt install -y kde-plasma-desktop --no-install-recommends
+sudo apt install -y kde-plasma-desktop plasma-discover plasma-discover-backend-fwupd plasma-discover-backend-flatpak --no-install-recommends
 
 # Install additional KDE applications
-sudo apt install -y firefox-esr plasma-nm plasma-systemmonitor sddm plasma-workspace-wayland ffmpeg* kio-extras dolphin-plugins kimageformat-plugins
+sudo apt install -y sddm plasma-nm plasma-systemmonitor plasma-workspace-wayland ffmpeg* kio-extras kimageformat-plugins dolphin-plugins firefox-esr
 
 # Check if the installation was successful
 if [ $? -eq 0 ]; then
