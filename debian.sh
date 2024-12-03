8989923899ba75b367663201fd22f64f15dc019c
NotIzxxY6677 <81343518+NotIzxxY6677@users.noreply.github.com>
2024-12-03 11:05:54 +0530

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Install essential packages
-sudo apt install -y intel-media-va-driver intel-opencl-icd intel-gpu-tools mesa-* openal-info libopenal-dev libglfw3-wayland libglfw3-dev p7zip-full
+sudo apt install -y intel-media-va-driver intel-opencl-icd intel-gpu-tools mesa-* openal-info libopenal-dev libglfw3-wayland libglfw3-dev p7zip-full vulkan-tools opencl-headers
 
 # Install KDE Plasma Desktop without recommended packages
 sudo apt install -y kde-plasma-desktop --no-install-recommends
 
 # Install additional KDE applications
-sudo apt install -y firefox-esr plasma-nm plasma-systemmonitor
+sudo apt install -y firefox-esr plasma-nm plasma-systemmonitor sddm plasma-workspace-wayland
 
 # Check if the installation was successful
 if [ $? -eq 0 ]; then
