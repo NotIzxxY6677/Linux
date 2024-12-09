409c63a391574ec98ffede97d8ab86c9e13bdd94
NotIzxxY6677 <81343518+NotIzxxY6677@users.noreply.github.com>
2024-12-09 23:54:51 +0530

    Update Debian12_KDE.sh

--- Debian12_KDE.sh
+++ Debian12_KDE.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Install essential packages
+# Install drivers-related packages
 sudo apt install -y intel-media-va-driver intel-opencl-icd intel-gpu-tools mesa-* openal-info libopenal-dev libglfw3-wayland libglfw3-dev p7zip-full vulkan-tools opencl-headers
 
 # Install KDE Plasma Desktop without recommended packages
 sudo apt install -y kde-plasma-desktop plasma-discover plasma-discover-backend-fwupd plasma-discover-backend-flatpak --no-install-recommends
 
-# Install additional KDE applications
+# Install additional KDE applications & others
 sudo apt install -y sddm plasma-nm plasma-systemmonitor plasma-workspace-wayland ffmpeg* kio-extras kimageformat-plugins dolphin-plugins firefox-esr
 
 # Check if the installation was successful
