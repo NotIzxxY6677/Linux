3dea8b0e4f704001c5602d9fba6bddd482b05669
NotIzxxY6677 <81343518+NotIzxxY6677@users.noreply.github.com>
2024-12-10 00:06:27 +0530

    Update ArchPostInstall.sh

--- ArchPostInstall.sh
+++ ArchPostInstall.sh
@@ -1,2 +1,8 @@
 #!bin/bash
 
+# Create a Normal User account RECOMMENDED TO DO MANUALLY
+#pacman -S sudo
+#useradd -m -G wheel -s /bin/bash user01
+#passwd user01
+#<password>
+# uncomment wheel from sudoers file using nano or vim.
