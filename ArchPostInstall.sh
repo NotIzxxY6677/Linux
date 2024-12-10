12461c5e5570cff957eea23dd5625faad4353e13
NotIzxxY6677 <81343518+NotIzxxY6677@users.noreply.github.com>
2024-12-10 22:48:20 +0530

    Update and rename ArchPostInstall.txt to ArchPostInstall.sh

---
+++ ArchPostInstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Create a Normal User account MANUALLY.
+pacman -S sudo
+#useradd -m -G wheel -s /bin/bash user01
+#passwd user01
+#<PASSWORD>
+#Uncomment wheel from sudoers file using nano(/etc/sudoers) or vim(visudo).
+
+#Install driver-related packages:
+sudo pacman -S mesa vulkan-intel intel-graphics-compiler intel-gmmlib intel-metee igsc intel-media-driver libva-utils rkcommon opencl-headers
+
+#Install printer related drivers/apps:
+sudo pacman -S print-manager cups system-config-printer
